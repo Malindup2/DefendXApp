92868ab7cf1f80b7fd2599021c27a8fac9a8d1cc
Malindu Pabasara <139533700+Malindup2@users.noreply.github.com>
2025-03-18 22:17:38 +0530

    completed up to get start

--- app/src/main/java/com/example/defendx/onBoradingTwo.kt
+++ app/src/main/java/com/example/defendx/onBoradingTwo.kt
@@ -1,4 +1,5 @@
 package com.example.defendx
+
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -9,10 +10,10 @@ class OnboardingActivityTwo : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboard2)
 
-        // Next arrow click listener
+
         val nextArrow = findViewById<ImageView>(R.id.next_arrow)
         nextArrow.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java) // Go to MainActivity
+            val intent = Intent(this, OnboardingActivityThree::class.java)
             startActivity(intent)
             finish()
         }

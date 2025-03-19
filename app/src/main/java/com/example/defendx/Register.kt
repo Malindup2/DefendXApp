041edc1700c8555b63e20908de509e5121d66257
Malindu Pabasara <139533700+Malindup2@users.noreply.github.com>
2025-03-19 21:47:40 +0530

    completed up to login

--- app/src/main/java/com/example/defendx/Register.kt
+++ app/src/main/java/com/example/defendx/Register.kt
@@ -5,15 +5,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 
-class Login : AppCompatActivity() {
+class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
 
 
-        val getStartedButton = findViewById<Button>(R.id.getStartedButton)
+        val getStartedButton = findViewById<Button>(R.id.registerbtn)
         getStartedButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, Login::class.java)
             startActivity(intent)
             finish()
         }
--- app/src/main/java/com/example/defendx/onBoardingThree.kt
+++ app/src/main/java/com/example/defendx/onBoardingThree.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.widget.TextView
 
 class OnboardingActivityThree : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +15,14 @@ class OnboardingActivityThree : AppCompatActivity() {
         val getStartedButton = findViewById<Button>(R.id.getStartedButton)
         getStartedButton.setOnClickListener {
             val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+        val dontHaveAccountText = findViewById<TextView>(R.id.sign_in)
+        dontHaveAccountText.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
             startActivity(intent)
             finish()
         }
